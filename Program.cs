b5c3057e05935fe10aa5d24c9342cd49fdc13643
Matt Obert on Windows <matt@hotwebmatter.com>
2019-09-01 22:22:09 -0400

    Initial commit of ASCII C# exercise

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace AsciiCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine();
+            WriteLine("   _____     _  _");
+            WriteLine("  / ____|  _| || |_");
+            WriteLine(" | |      |_  __  _|");
+            WriteLine(" | |       _| || |_");
+            WriteLine(" | |____  |_  __  _|");
+            WriteLine("  \\_____|   |_||_|");
+            WriteLine();
+            ReadKey();
+        }
+    }
+}
